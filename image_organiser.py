89921d71409f7bd93b3baf9b265901645a0a575d
Philip Scadding <poscadding@hotmail.co.uk>
2017-07-29 09:13:27 +0100

    no message

--- image_organiser.py
+++ image_organiser.py
@@ -8,18 +8,21 @@ import exifread
 from datetime import datetime
 
 logger = logging.getLogger('Photo_Organiser')
-logging.basicConfig()
+logging.basicConfig(stream=sys.stdout)
 logger.setLevel(logging.DEBUG)
+
 
 tag_match_list = ['Holiday','Events','Outings','Birthday']
 
-root_path = r"F:\Users\Pictures\Photos\Holidays"
+# root_path = r"F:\Users\Pictures\Photos\Holidays"
+root_path = r"/Volumes/Seagate Expansion Drive/Users/Pictures/Photos/Holidays"
+dest_folder = r"/Volumes/Seagate Expansion Drive/Users/Pictures/Photos"
 
-
+files_not_copied = []
 
 def find_tags(aFile, tag_list):
-    fd = open(file_path, encoding="Latin-1")
-    d = fd.read()
+    with open(aFile, encoding="Latin-1") as fd:
+        d = fd.read()
     xmp_start = d.find('<x:xmpmeta')
     xmp_end = d.find('</x:xmpmeta')
     xmp_str = d[xmp_start:xmp_end + 12]
@@ -41,21 +44,54 @@ def get_date_time(data):
     logger.debug("%s %s" % (date_times,datetime_object))
     return datetime_object
 
+def construct_new_path(file,tags,datetime):
+
+    if tags:
+        path = os.sep.join([str(datetime.year),datetime.strftime("%B")] + tags)
+    else:
+        path = datetime.strftime("%Y\\%B\\%d")
+
+    path = os.path.join(path,file)
+    logger.info(path)
+
+    return path
+
+
+
 for root, folders, files in os.walk(root_path):
     logger.info("Root: %s" % root)
     for aFile in files:
-        file_path = os.path.join(root,aFile)
-        logger.info("%s" % file_path)
+        source_path = os.path.join(root,aFile)
+        logger.info("%s" % source_path)
 
-        tags = find_tags(file_path,tag_match_list)
 
-        f = open(file_path, 'rb')
+        with open(source_path, 'rb') as f:
 
-        # Return Exif tags
-        exif_tags = exifread.process_file(f)
-        # pprint.pprint(exif_tags)
+            tags = find_tags(source_path,tag_match_list)
 
-        logger.debug("Tags: %s" % tags)
-        get_date_time(exif_tags)
+            # Return Exif tags
+            try:
+                exif_tags = exifread.process_file(f)
+                # pprint.pprint(exif_tags)
+
+                logger.debug("Tags: %s" % tags)
+                date_time = get_date_time(exif_tags)
+            except OSError as e:
+                logger.warning("Failed to read exif data: %s" % e)
+                files_not_copied.append(source_path)
+                continue
+
+            if date_time is None:
+                files_not_copied.append(source_path)
+                continue
+
+        new_path = construct_new_path(aFile, tags, date_time)
+        new_path = os.path.join(dest_folder,new_path)
+        logger.info(new_path)
+
+        #handle clashes with file name
+
+
         logger.info("\n\n\n")
 
+logger.info("Files not copied: %s" % files_not_copied)
